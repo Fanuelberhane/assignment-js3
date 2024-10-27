cd1a683b65b0cd983b62483d3e76fa71a0d579ca
Fanuelberhane <fanuelberhanek@gmail.com>
2024-10-27 15:46:57 +0300

    Add files via upload

---
+++ added javascript.js
@@ -0,0 +1,8 @@
+function addJavaScript(str) {
+    return "JavaScript " + str;
+}
+
+const originalString = "is a programing language.";
+const newString = addJavaScript(originalString);
+
+console.log(newString); 
